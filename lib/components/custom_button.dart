22c8054b91330502322e77f21e4d229c2b19054d
YunShin <2younsin@gmail.com>
2023-02-26 17:35:33 +0900

    외부 icon 설치 & Custom_button 생성 중

---
+++ lib/components/custom_button.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+
+// class CustomButton extends StatelessWidget {
+//   const CustomButton({super.key});
+
+//   final Function()? onTab;
+//   final String title;
+//   final String detail;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }

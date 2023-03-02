24de13d3adb382cd4905bd2ef649c1edba3f1fe3
YunShin <2younsin@gmail.com>
2023-03-02 12:17:46 +0900

    MonitorSettingsScreen 생성
    스마트폰 보유 갯수에 따라 단일디바이스 vs 원격디바이스 모드를 선택하는 Screen

---
+++ lib/screens/monitor_settings_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../commons/background.dart';
+import '../components/custom_button.dart';
+
+class MonitorSettingsScreen extends StatelessWidget {
+  const MonitorSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            const Background(),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomButton(
+                    onTab: () => {},
+                    title: "단일 다바이스",
+                    detail: "모바일 장치가 하나입니다!",
+                    svgIcon: SvgPicture.asset(
+                      'assets/icons/router.svg',
+                      width: 60,
+                      height: 60,
+                    ),
+                  ),
+                  CustomButton(
+                    onTab: () => {},
+                    title: "원격 디바이스",
+                    detail: "모바일 장치가\n두 개 이상입니다!",
+                    svgIcon: SvgPicture.asset(
+                      'assets/icons/router.svg',
+                      width: 60,
+                      height: 60,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
